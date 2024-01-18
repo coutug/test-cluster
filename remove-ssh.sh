613a777bd28827616270c769f1e1243775aca579
coutug <coutug18@hotmail.com>
2024-01-18 09:34:52 -0500

    clean everything

--- remove-ssh.sh
+++ remove-ssh.sh
@@ -6,11 +6,12 @@ ssh-keygen -R k0s-ctl3-dom114.mar.eosn.io
 ssh-keygen -R dom115.mar.eosn.io
 ssh-keygen -R dom116.mar.eosn.io
 
-ssh-keygen -R "172.22.3.209"
-ssh-keygen -R "172.22.3.233"
-ssh-keygen -R "172.22.3.246"
+ssh-keygen -R "172.22.126.51"
+ssh-keygen -R "172.22.126.52"
+ssh-keygen -R "172.22.126.53"
 ssh-keygen -R "172.22.1.115"
 ssh-keygen -R "172.22.1.116"
+ssh-keygen -R "172.22.126.11"
 
 ssh-keygen -R k0s-ctl1-dom4.lab.eosn.io
 ssh-keygen -R k0s-ctl2-dom4.lab.eosn.io
